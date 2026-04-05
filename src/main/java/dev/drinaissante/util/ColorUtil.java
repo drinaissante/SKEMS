e08b6f56d45d433510f9c59fb3fb1f24db9a5985
drinaissante <victoriaaldrin812@gmail.com>
2026-04-05 18:54:37 +0800

    Added ColorUtil that gets hex from Color

---
+++ src/main/java/dev/drinaissante/util/ColorUtil.java
@@ -0,0 +1,13 @@
+package dev.drinaissante.util;
+
+import javafx.scene.paint.Color;
+
+public class ColorUtil {
+    public static String toHexCode(Color color) {
+        int r = (int) Math.round(color.getRed() * 255);
+        int g = (int) Math.round(color.getGreen() * 255);
+        int b = (int) Math.round(color.getBlue() * 255);
+
+        return String.format("#%02X%02X%02X", r, g, b);
+    }
+}
