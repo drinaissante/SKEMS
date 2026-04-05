705471fc15967f969f59f27d3fdbe6c3d376367c
drinaissante <victoriaaldrin812@gmail.com>
2026-04-05 18:55:12 +0800

    Made the scroll pane background properly

--- src/main/java/dev/drinaissante/tabs/EquipmentsTab.java
+++ src/main/java/dev/drinaissante/tabs/EquipmentsTab.java
@@ -1,6 +1,9 @@
 package dev.drinaissante.tabs;
 
 import dev.drinaissante.Main;
+import dev.drinaissante.util.ColorUtil;
+import dev.drinaissante.util.Fonts;
+import javafx.animation.ScaleTransition;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Cursor;
@@ -14,15 +17,18 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Popup;
+import javafx.util.Duration;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.Random;
 
 public class EquipmentsTab implements SKTab {
 
     private final List<String> STATUS = List.of("Available", "Checked Out", "Pending", "Unavailable");
     private final Image searchImg = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/search.png")));
     private final BorderPane mainContent = new BorderPane();
+    private final Image image = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/camera_1.png")));
 
     @Override
     public Node getNode() {
@@ -125,17 +131,20 @@ public class EquipmentsTab implements SKTab {
 
     private ScrollPane setupEquipmentScrollPane() {
         TilePane equipmentGrid = new TilePane();
-        equipmentGrid.setPrefColumns(4);
+        equipmentGrid.setPrefColumns(5);
         equipmentGrid.setHgap(20);
         equipmentGrid.setVgap(20);
-        equipmentGrid.setPadding(new Insets(20));
+        equipmentGrid.setPadding(new Insets(10));
         equipmentGrid.setAlignment(Pos.TOP_LEFT);
+
+        // TODO temp
+        Random rand = new Random();
 
         for (int i = 0; i < 20; i++) {
             // TODO database
             String name = "Sony Mavica";
             String owner = "Yow momma";
-            String status = "AVAILABLE";
+            String status = rand.nextBoolean() ? "AVAILABLE" : "PENDING";
 
             VBox item = getItem(status, name, owner);
 
@@ -143,12 +152,7 @@ public class EquipmentsTab implements SKTab {
         }
 
         ScrollPane scrollPane = new ScrollPane(equipmentGrid);
-        scrollPane.setStyle(
-                "-fx-background-color: transparent;" +
-                        "-fx-background-color: transparent;" +
-                        "-fx-control-inner-background: transparent;"
-        );
-        scrollPane.setBackground(Background.EMPTY);
+        scrollPane.getStyleClass().add("scroll-pane");
         scrollPane.setFitToWidth(true);
         scrollPane.setFitToHeight(true);
         scrollPane.setPannable(true);
@@ -175,18 +179,37 @@ public class EquipmentsTab implements SKTab {
 
     // TODO get the image from database
     private VBox getItem(String status, String name, String owner) {
-        VBox item = new VBox(20);
+        VBox item = new VBox(10);
         item.getStyleClass().add("equipment-item");
 
+        ScaleTransition st = new ScaleTransition(Duration.millis(200), item);
+        st.setFromX(1.0);
+        st.setFromY(1.0);
+        st.setToX(1.1);
+        st.setToY(1.1);
+
+        item.setOnMouseEntered(event -> {
+            st.setRate(1);
+            st.playFromStart();
+        });
+        item.setOnMouseExited(event -> {
+            st.setRate(-1);
+            st.play();
+        });
+
         // TODO fetch image | add skeleton
-        Image image = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/camera_1.png")));
         ImageView imageView = new ImageView(image);
         imageView.setFitHeight(80);
         imageView.setFitWidth(80);
         imageView.setPreserveRatio(true);
 
-        HBox imageCenter = new HBox(imageView);
-        imageCenter.setAlignment(Pos.CENTER);
+        Label statusLbl = getStatusLbl(status);
+        HBox statusHBox = new HBox(statusLbl);
+        statusHBox.setAlignment(Pos.CENTER);
+
+        // StackPane: image centered, label top-left
+        StackPane imagePane = new StackPane(imageView);
+        StackPane.setAlignment(imagePane, Pos.CENTER);
 
         Text nameText = new Text(name);
         Text ownerText = new Text("Owner: " + owner);
@@ -194,24 +217,117 @@ public class EquipmentsTab implements SKTab {
         VBox nameBox = new VBox(nameText, ownerText);
         nameBox.setAlignment(Pos.CENTER_LEFT);
 
-        item.getChildren().addAll(imageCenter, nameBox);
+        item.getChildren().addAll(statusHBox, imagePane, nameBox);
 
         item.setCursor(Cursor.HAND);
-        Popup popup = new Popup();
-        VBox popupContent = new VBox();
 
-        Label text = new Label("Details for " + name);
-        text.setTextFill(Color.WHITE);
-
-        popupContent.getChildren().add(text);
-
-        popupContent.setOnMouseExited(event -> popup.hide());
-
-        popupContent.setStyle("-fx-background-color: rgba(0,0,0,0.6); -fx-padding: 40;");
-        popup.getContent().add(popupContent);
-        popup.setAutoHide(true);
+        Popup popup = generatePopup(status, name, owner);
 
         item.setOnMouseClicked(e -> popup.show(Main.STAGE, e.getScreenX() + 10, e.getScreenY()));
         return item;
+    }
+
+    private Label getStatusLbl(String status) {
+        status = status.toUpperCase();
+        Label statusLbl = new Label(status);
+
+        String valueFill = ColorUtil.toHexCode(Color.GRAY);
+
+        switch (status) {
+            case "AVAILABLE":
+                valueFill = "#7ed957";
+                break;
+            case "CHECKED_OUT":
+                valueFill = ColorUtil.toHexCode(Color.YELLOW);
+                break;
+            case "PENDING":
+                valueFill = ColorUtil.toHexCode(Color.ORANGERED);
+                break;
+            case "OVERDUE":
+                valueFill = "#ff3131";
+                break;
+            case "UNAVAILABLE":
+                valueFill = "#545454";
+            default:
+                break;
+        }
+
+        statusLbl.setStyle("""
+                -fx-padding: 0 15 0 15;
+                -fx-background-radius: 12;
+                -fx-background-color: %s;
+                -fx-text-fill: white;
+                -fx-font-weight: bold;
+                """.formatted(valueFill)
+        );
+
+        return statusLbl;
+    }
+
+    private Popup generatePopup(String status, String name, String owner) {
+        // popup
+        Popup popup = new Popup();
+        VBox popupContent = new VBox(15);
+        popupContent.setStyle("-fx-background-color: rgba(0,0,0,0.6); -fx-padding: 20;");
+
+        // TODO the picture itself
+        HBox camWithInfo = new HBox(10);
+        camWithInfo.setStyle("-fx-background-color: gray; -fx-padding: 15; -fx-background-radius: 12;");
+
+        ImageView imageView2 = new ImageView(image);
+        imageView2.setFitWidth(100);
+        imageView2.setFitHeight(100);
+        imageView2.setPreserveRatio(true);
+
+        Label statusLbl = getStatusLbl(status);
+
+        Text nameText = new Text(name);
+        nameText.setFill(Color.WHITE);
+        nameText.setFont(Font.font(Fonts.COMIC_NEUE_BOLD, FontWeight.BOLD, 16));
+        Text ownerText = new Text("Owner: " + owner);
+        ownerText.setFill(Color.WHITE);
+        ownerText.setFont(Font.font(Fonts.COMIC_NEUE_BOLD, FontWeight.BOLD, 16));
+
+        HBox nameWithStatus = new HBox(10, nameText, statusLbl);
+
+        VBox textBox = new VBox(10, nameWithStatus, ownerText);
+        textBox.setAlignment(Pos.CENTER_LEFT);
+
+        camWithInfo.getChildren().addAll(imageView2, textBox);
+
+        // TODO notes (to add in database)
+        HBox noteWithQR = new HBox(15);
+
+        Label noteText = new Label("THE NOTE HERE\nWITH NOTES AND THIS\n*\na\na\na\na\na\na\na\na\na\na\na");
+        noteText.setFont(Font.font(Fonts.COMIC_NEUE_BOLD, FontWeight.BOLD, 16));
+        noteText.setStyle("-fx-background-color: gray; -fx-text-fill: white; -fx-padding: 10; -fx-background-radius: 10;");
+
+        VBox qrBox = new VBox(10);
+        qrBox.setAlignment(Pos.CENTER);
+        Label qrLbl = new Label("QR Code");
+        qrLbl.setStyle("""
+                -fx-padding: 0 15 0 15;
+                -fx-background-radius: 8;
+                -fx-background-color: #a6a6a6;
+                -fx-text-fill: white;
+                """);
+
+        // TODO the qr image here
+        ImageView qrImageView = new ImageView(image);
+        qrImageView.setFitWidth(100);
+        qrImageView.setFitHeight(100);
+        qrImageView.setPreserveRatio(true);
+
+        qrBox.getChildren().addAll(qrLbl, qrImageView);
+
+        noteWithQR.getChildren().addAll(noteText, qrBox);
+
+        popupContent.getChildren().addAll(camWithInfo, noteWithQR);
+        popupContent.setOnMouseExited(event -> popup.hide());
+
+        popup.getContent().add(popupContent);
+        popup.setAutoHide(true);
+
+        return popup;
     }
 }
